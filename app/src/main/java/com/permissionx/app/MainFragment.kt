ed20f611d6bf70a05a206186fef551197d3e9f5f
wangming <wangming@yuaiweiwu.com>
2025-03-20 17:39:06 +0800

    feat: 支持申请权限的同时增加权限说明提示
    (cherry picked from commit 9dfa84b8f5f735badeb1681f99654185a471a76e)

--- app/src/main/java/com/permissionx/app/MainFragment.kt
+++ app/src/main/java/com/permissionx/app/MainFragment.kt
@@ -2,33 +2,43 @@ package com.permissionx.app
 
 import android.Manifest
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.permissionx.app.databinding.FragmentMainBinding
 import com.permissionx.guolindev.PermissionX
+import com.permissionx.guolindev.dialog.DefaultDialog
+import com.permissionx.guolindev.dialog.PermissionTipsView
 
 class MainFragment : Fragment() {
 
     private var _binding: FragmentMainBinding? = null
 
     private val binding get() = _binding!!
+    var permissionTipsView: PermissionTipsView? = null
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 //        val context = context!!
         binding.makeRequestBtn.setOnClickListener {
             PermissionX.init(this)
                 .permissions(
-                Manifest.permission.CAMERA,
+                    Manifest.permission.CAMERA,
                     Manifest.permission.ACCESS_FINE_LOCATION,
                     Manifest.permission.RECORD_AUDIO,
 //                    Manifest.permission.READ_CALENDAR,
@@ -41,22 +51,28 @@ class MainFragment : Fragment() {
 //                    Manifest.permission.READ_EXTERNAL_STORAGE
                 )
                 .setDialogTintColor(Color.parseColor("#1972e8"), Color.parseColor("#8ab6f5"))
-                .onExplainRequestReason { scope, deniedList, beforeRequest ->
-                    val message = "PermissionX needs following permissions to continue"
-                    scope.showRequestReasonDialog(deniedList, message, "Allow", "Deny")
-//                    val message = "Please allow the following permissions in settings"
-//                    val dialog = CustomDialogFragment(message, deniedList)
-//                    scope.showRequestReasonDialog(dialog)
+                .explainReasonWhenRequest()
+                .onExplainRequestReasonWhenRequest { scope, deniedList ->
+                    permissionTipsView = PermissionTipsView.Builder(this@MainFragment.requireContext()).setTitle("温馨提示")
+                        .setIcon(R.drawable.ic_launcher_background)
+                        .setMessage("申请相机权限用于视频通话").setOrientation(true).build()
+                    permissionTipsView?.show()
                 }
                 .onForwardToSettings { scope, deniedList ->
                     val message = "Please allow following permissions in settings"
                     scope.showForwardToSettingsDialog(deniedList, message, "Allow", "Deny")
                 }
                 .request { allGranted, grantedList, deniedList ->
+                    permissionTipsView?.dismiss()
                     if (allGranted) {
-                        Toast.makeText(activity, "All permissions are granted", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(activity, "All permissions are granted", Toast.LENGTH_SHORT)
+                            .show()
                     } else {
-                        Toast.makeText(activity, "The following permissions are denied：$deniedList", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            activity,
+                            "The following permissions are denied：$deniedList",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
                 }
         }
