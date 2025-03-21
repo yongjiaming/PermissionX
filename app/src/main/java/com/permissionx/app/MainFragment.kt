85080b932c89788973706b842cc5c7e5f14006f1
wangming <wangming@yuaiweiwu.com>
2025-03-21 15:28:14 +0800

    chore: update demo

--- app/src/main/java/com/permissionx/app/MainFragment.kt
+++ app/src/main/java/com/permissionx/app/MainFragment.kt
@@ -35,7 +35,44 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 //        val context = context!!
+
         binding.makeRequestBtn.setOnClickListener {
+            PermissionX.init(this)
+                .permissions(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.RECORD_AUDIO,
+//                    Manifest.permission.READ_CALENDAR,
+//                    Manifest.permission.READ_CALL_LOG,
+//                    Manifest.permission.READ_CONTACTS,
+//                    Manifest.permission.READ_PHONE_STATE,
+//                    Manifest.permission.BODY_SENSORS,
+//                    Manifest.permission.ACTIVITY_RECOGNITION,
+//                    Manifest.permission.SEND_SMS,
+//                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+                .setDialogTintColor(Color.parseColor("#1972e8"), Color.parseColor("#8ab6f5"))
+                .onExplainRequestReason { scope, deniedList, beforeRequest ->
+                    val message = "PermissionX needs following permissions to continue"
+                    scope.showRequestReasonDialog(deniedList, message, "Allow", "Deny")
+//                    val message = "Please allow the following permissions in settings"
+//                    val dialog = CustomDialogFragment(message, deniedList)
+//                    scope.showRequestReasonDialog(dialog)
+                }
+                .onForwardToSettings { scope, deniedList ->
+                    val message = "Please allow following permissions in settings"
+                    scope.showForwardToSettingsDialog(deniedList, message, "Allow", "Deny")
+                }
+                .request { allGranted, grantedList, deniedList ->
+                    if (allGranted) {
+                        Toast.makeText(activity, "All permissions are granted", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(activity, "The following permissions are denied：$deniedList", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+
+        binding.makeRequestBtn2.setOnClickListener {
             PermissionX.init(this)
                 .permissions(
                     Manifest.permission.CAMERA,
@@ -56,6 +93,48 @@ class MainFragment : Fragment() {
                     permissionTipsView = PermissionTipsView.Builder(this@MainFragment.requireContext()).setTitle("相机权限申请")
                         .setIcon(R.drawable.ic_launcher_background)
                         .setMessage("申请相机权限用于视频通话").setOrientation(false).build()
+                    permissionTipsView?.show()
+                }
+                .onForwardToSettings { scope, deniedList ->
+                    val message = "Please allow following permissions in settings"
+                    scope.showForwardToSettingsDialog(deniedList, message, "Allow", "Deny")
+                }
+                .request { allGranted, grantedList, deniedList ->
+                    permissionTipsView?.dismiss()
+                    if (allGranted) {
+                        Toast.makeText(activity, "All permissions are granted", Toast.LENGTH_SHORT)
+                            .show()
+                    } else {
+                        Toast.makeText(
+                            activity,
+                            "The following permissions are denied：$deniedList",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+
+        binding.makeRequestBtn3.setOnClickListener {
+            PermissionX.init(this)
+                .permissions(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.RECORD_AUDIO,
+//                    Manifest.permission.READ_CALENDAR,
+//                    Manifest.permission.READ_CALL_LOG,
+//                    Manifest.permission.READ_CONTACTS,
+//                    Manifest.permission.READ_PHONE_STATE,
+//                    Manifest.permission.BODY_SENSORS,
+//                    Manifest.permission.ACTIVITY_RECOGNITION,
+//                    Manifest.permission.SEND_SMS,
+//                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+                .setDialogTintColor(Color.parseColor("#1972e8"), Color.parseColor("#8ab6f5"))
+                .explainReasonWhenRequest()
+                .onExplainRequestReasonWhenRequest { scope, deniedList ->
+                    permissionTipsView = PermissionTipsView.Builder(this@MainFragment.requireContext()).setTitle("相机权限申请")
+                        .setIcon(R.drawable.ic_launcher_background)
+                        .setMessage("申请相机权限用于视频通话").setOrientation(true).build()
                     permissionTipsView?.show()
                 }
                 .onForwardToSettings { scope, deniedList ->
